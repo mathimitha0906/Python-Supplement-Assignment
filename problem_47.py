ae26b73bd566465d4da97c317d7b159744b3101c
mathimitha0906 <jeevamathi934@gmail.com>
2026-01-30 14:19:30 +0530

    problem_47.py is solved

--- problem_47.py
+++ problem_47.py
@@ -2,10 +2,11 @@
 # Find and fix the error
 
 def starts_with(text, char):
-    if text[0] == char:
+    if text and text[0] == char:  # Check text is not empty
         return True
     return False
 
 word = "Python"
 print(f"Starts with 'P': {starts_with(word, 'P')}")
 print(f"Starts with 'J': {starts_with('', 'J')}")
+
