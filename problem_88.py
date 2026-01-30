ddf49c608c50238c812e2a99b1cad693cd5d82c9
mathimitha0906 <jeevamathi934@gmail.com>
2026-01-30 15:17:14 +0530

    problem_88.py is solved

--- problem_88.py
+++ problem_88.py
@@ -1,10 +1,11 @@
 # Problem 88: Find all substrings of a string
 # Find and fix the error
 
+
 def all_substrings(text):
     substrings = []
     for i in range(len(text)):
-        for j in range(i+1, len(text)):
+        for j in range(i+1, len(text)+1):  # +1 to include last character
             substrings.append(text[i:j])
     return substrings
 
